43b871fd2c0a581c73c968cbdfaff0bca452d984
WaterThePotatoes <1yinton@hdsb.ca>
2026-01-24 22:55:04 -0500

    release update

--- release/release-config.js
+++ release/release-config.js
@@ -1,13 +1,13 @@
 export const STRATA_RELEASE = {
   pageUrl: "https://github.com/despotato/strata_releases",
   mac: {
-    dmgUrl: "https://github.com/despotato/strata_releases/releases/download/v1.2.0/Strata-1.2.0-arm64.dmg",
+    dmgUrl: "https://github.com/despotato/strata_releases/releases/download/v1.2.1/Strata-1.2.1-arm64.dmg",
     zipUrl: ""
   },
   windows: {
-    exeUrl: "https://github.com/despotato/strata_releases/releases/download/v1.2.0/Strata.Setup.1.2.0.exe"
+    exeUrl: "https://github.com/despotato/strata_releases/releases/download/v1.2.1/Strata.Setup.1.2.1.exe"
   },
-  version: "v1.2.0",
+  version: "v1.2.1",
   date: "2026-01-24"
 };
 
